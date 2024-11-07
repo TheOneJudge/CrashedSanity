6435be67d1879f0f6874719a022b4b10fb768d12
veryReallyGoodCoder <6000176@sae.edu.za>
2024-11-07 14:31:34 +0200

    Yairon Scene | PressE Scrolling Menu Added

--- Assets/Yairon/Scripts/ComputerInterface.cs
+++ Assets/Yairon/Scripts/ComputerInterface.cs
@@ -5,32 +5,55 @@ public class ComputerInterface : MonoBehaviour
 {
     public UnityEvent myEvent;
     
-    [SerializeField] private GameObject canvas, cam;
+    [SerializeField] private GameObject canvas, menu;
+    [SerializeField] private GameObject cam;
+
+    private bool triggered = false;
+    private bool ePress = false;
+
 
     private void Start()
     {
         canvas.SetActive(false);
+        menu.SetActive(false);
     }
 
 
     private void Update()
     {
-        canvas.transform.localRotation = Camera.main.transform.localRotation;
+        canvas.transform.localRotation = cam.transform.localRotation;
+
+        Debug.Log(cam.transform.localRotation);
+
+        if (triggered && Input.GetKeyUp(KeyCode.E))
+        {
+            ePress = true;
+            ShowMenu(ePress);
+        }
+        
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            ShowE(true);
+            
+            triggered = true;
+            ShowE(triggered);
             Debug.Log("player enter");
+
+            
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        
         if (other.tag == "Player")
         {
-            ShowE(false);
+            triggered = false;
+            ShowE(triggered);
+            ShowMenu(triggered);
             Debug.Log("player exit");
         }
     }
@@ -42,7 +65,28 @@ public class ComputerInterface : MonoBehaviour
         }
         else
         {
-            canvas.SetActive(false);
+            canvas.SetActive(triggered);
         }
+
+        //ePress = triggered;
+    }
+
+    private void ShowMenu(bool ePress)
+    {
+
+        if (ePress)
+        {
+            ShowE(false);
+            menu.SetActive(ePress);
+
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            menu.SetActive(false);
+            Cursor.lockState= CursorLockMode.Locked;
+        }
+
+        Cursor.visible = ePress;
     }
 }
