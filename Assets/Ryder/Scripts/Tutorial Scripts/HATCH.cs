cb05f5cfa2578df11e00ff4e94ff98b8de434fb9
DishonoredNut <xmaskillee@gmail.com>
2024-11-11 22:03:37 +0200

    updated hatch script

--- Assets/Ryder/Scripts/Tutorial Scripts/HATCH.cs
+++ Assets/Ryder/Scripts/Tutorial Scripts/HATCH.cs
@@ -4,25 +4,17 @@ using UnityEngine;
 
 public class HATCH : MonoBehaviour
 {
-    public string playerTag = "Player";  // Tag for the player (you can set this in the Inspector or keep it hardcoded)
+    public string playerTag = "Player";  
 
     // Trigger collision detection with the player
     void OnTriggerEnter(Collider other)
     {
-        // Check if the object colliding has the "Player" tag
+        
         if (other.CompareTag(playerTag))
         {
-            // Destroy this game object upon collision with the player
-            Destroy(gameObject);  // This deletes the object this script is attached to
+            
+            Destroy(gameObject);  
         }
     }
 
-    // Optionally, you can use OnCollisionEnter if you need non-trigger colliders
-    // void OnCollisionEnter(Collision collision)
-    // {
-    //     if (collision.gameObject.CompareTag(playerTag))
-    //     {
-    //         Destroy(gameObject);  // This deletes the object this script is attached to
-    //     }
-    // }
-}
+    
